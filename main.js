dcef7f6cdf0a3d56da98c5e25c8f22ca3a12c28b
kaizju <rookieslit@gmail.com>
2026-08-17 12:46:08 +0800

    added electron

---
+++ main.js
@@ -0,0 +1,27 @@
+// main.js (Electron main process)
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
+const path = require('path');
+
+let phpServer;
+
+function startPhpServer() {
+    const phpPath = 'C:\\xampp\\php\\php.exe'; // XAMPP's actual PHP location
+    const docRoot = path.join(__dirname, '..', 'hopeline'); // adjust to wherever your hopeline folder actually is relative to this Electron project
+    phpServer = spawn(phpPath, ['-S', 'localhost:8000', '-t', docRoot]);
+}
+
+function createWindow() {
+    const win = new BrowserWindow({ width: 1280, height: 800 });
+    win.loadURL('http://localhost:8000/index.php');
+}
+
+app.whenReady().then(() => {
+    startPhpServer();
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (phpServer) phpServer.kill();
+    app.quit();
+});
